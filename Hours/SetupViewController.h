0b3b384b08ebabcd42eba9a2368174f30b6ac6c9
Tommy Wendelborg <wendelborg@gmail.com>
2012-11-09 22:34:54 +0100

    fake login and logout works :)

---
+++ Hours/SetupViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetupViewController.h
+//  Hours
+//
+//  Created by Tommy Wendelborg on 11/9/12.
+//  Copyright (c) 2012 steria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetupViewController : UIViewController
+
+@end
---
+++ Hours/SetupViewController.m
@@ -0,0 +1,47 @@
+//
+//  SetupViewController.m
+//  Hours
+//
+//  Created by Tommy Wendelborg on 11/9/12.
+//  Copyright (c) 2012 steria. All rights reserved.
+//
+
+#import "SetupViewController.h"
+
+@interface SetupViewController ()
+- (IBAction)logOut:(id)sender;
+
+@end
+
+@implementation SetupViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+}
+- (IBAction)logOut:(id)sender
+{
+    UITabBarController *parent = (UITabBarController *)[self parentViewController];
+    [parent dismissViewControllerAnimated:YES completion:nil];
+}
+@end
