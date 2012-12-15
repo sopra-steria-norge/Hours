e9ca4a24816c6604b00f5a0e9b6d83851866d101
Tommy Wendelborg <wendelborg@gmail.com>
2012-12-15 21:28:43 +0100

    Added some more tests to the navigation logic, defined a custom assert for matching up date strings and dates

--- HoursTests/AppStateTests.h
+++ HoursTests/AppStateTests.h
@@ -10,6 +10,12 @@
 #import <SenTestingKit/SenTestingKit.h>
 #import "AppState.h"
 
+#define STAssertDateStringEqualsDate(expectedDateString, date) \
+do { \
+NSDate *expected = [self.formatter dateFromString:expectedDateString];\
+STAssertTrue([expected isEqualToDate:date], @"Expected date %@, got: %@", expectedDateString, [self.formatter stringFromDate:date]);\
+} while(0)
+
 @interface AppStateTests : SenTestCase
 
 @end
--- HoursTests/AppStateTests.m
+++ HoursTests/AppStateTests.m
@@ -18,6 +18,12 @@
 @synthesize formatter = _formatter;
 @synthesize appState = _appState;
 
+NSString * const twoDaysFromNowString   = @"20121212";
+NSString * const tomorrowString         = @"20121211";
+NSString * const todayString            = @"20121210";
+NSString * const yesterdayString        = @"20121209";
+NSString * const twoDaysAgoString       = @"20121208";
+
 - (void)setUp
 {
     [super setUp];
@@ -26,83 +32,120 @@
     self.formatter.timeStyle = NSDateFormatterNoStyle;
     self.formatter.dateFormat = @"yyyyMMdd";
     
-    self.appState = [[AppState alloc] initWithDate:self.today];
-    self.appState.currentWeek = [self testWeek];
+    self.appState = [[AppState alloc] initWithDate:[self.formatter dateFromString:todayString]];
+    self.appState.currentWeek = [self createTestWeekWithYesterdayTodayAndTomorrow];
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
     [self setFormatter:nil];
+    [self setAppState:nil];
     [super tearDown];
 }
 
 - (void)testMustReturnTodayAsCurrentDate
 {
     NSDate *currentDay = self.appState.currentDate;
-    NSDate *today = self.today;
+    NSDate *expected = [self.formatter dateFromString:todayString];
     
-    STAssertTrue([today isEqualToDate:currentDay], @"Current day was not initialized with %@, got: %@",
-                 [self.formatter stringFromDate:today], [self.formatter stringFromDate:currentDay]);
+    STAssertTrue([expected isEqualToDate:currentDay], @"Current day was not initialized with %@, got: %@",
+                 todayString, [self.formatter stringFromDate:currentDay]);
+}
+
+- (void) testSetupMustHavePeriodWithThreeDaysAndCurrentDayInTheMiddle
+{
+    NSDate *twoDaysFromNow = [self.formatter dateFromString:twoDaysFromNowString];
+    NSDate *tomorrow = [self.formatter dateFromString:tomorrowString];
+    NSDate *today = [self.formatter dateFromString:todayString];
+    NSDate *yesterday = [self.formatter dateFromString:yesterdayString];
+    NSDate *twoDaysAgo = [self.formatter dateFromString:twoDaysAgoString];
+    
+    STAssertTrue([today isEqualToDate:self.appState.currentDate], @"Bad test setup");
+    STAssertNotNil(self.appState.currentDay, @"Bad test setup");
+    STAssertNotNil([self.appState getDayForDate:tomorrow], @"Bad test setup");
+    STAssertNotNil([self.appState getDayForDate:yesterday], @"Bad test setup");
+                    
+    STAssertNil([self.appState getDayForDate:twoDaysFromNow], @"Bad test setup");
+    STAssertNil([self.appState getDayForDate:twoDaysAgo], @"Bad test setup");
 }
 
 - (void)testMustReturnYesterdayAsPreviousDate
 {
     NSDate *previousDate = self.appState.previousDate;
-    NSDate *yesterday = self.yesterday;
     
-    STAssertTrue([yesterday isEqualToDate:previousDate], @"Current day was not initialized with %@, got: %@",
-                 [self.formatter stringFromDate:yesterday], [self.formatter stringFromDate:previousDate]);
+    STAssertDateStringEqualsDate(yesterdayString, previousDate);
 }
 
 - (void)testMustReturnTomorrowAsNextDate
 {
     NSDate *nextDate = self.appState.nextDate;
-    NSDate *tomorrow = self.tomorrow;
-    
-    STAssertTrue([tomorrow isEqualToDate:nextDate], @"Current day was not initialized with %@, got: %@",
-                 [self.formatter stringFromDate:tomorrow], [self.formatter stringFromDate:nextDate]);
+    STAssertDateStringEqualsDate(tomorrowString, nextDate);
 }
 
 - (void) testNavigateNextDay_mustSetCurrentDateToTomorrow
 {
     AppState *updatedState = [self.appState navigateNextDay];
-    NSDate *tomorrow = self.tomorrow;
-    STAssertTrue([tomorrow isEqualToDate:updatedState.currentDate], @"Next day was not initialized with %@, got: %@",
-                 [self.formatter stringFromDate:tomorrow], [self.formatter stringFromDate:updatedState.currentDate]);
+    STAssertDateStringEqualsDate(tomorrowString, updatedState.currentDate);
 }
 
-- (void) testNavigatePreviosDay_mustSetCurrentDateToYesterday
+- (void) testNavigatePreviousDay_mustSetCurrentDateToYesterday
 {
     AppState *updatedState = [self.appState navigatePreviousDay];
-    NSDate *yesterday = self.yesterday;
-    STAssertTrue([yesterday isEqualToDate:updatedState.currentDate], @"Next day was not initialized with %@, got: %@",
-                 [self.formatter stringFromDate:yesterday], [self.formatter stringFromDate:updatedState.currentDate]);
+    STAssertDateStringEqualsDate(yesterdayString, updatedState.currentDate);
 }
 
-- (NSDate *)today
+- (void) testNavigatePreviousDay_mustSetCurrentWeekToNil_givenNoPreviousDayExists
 {
-    return [self.formatter dateFromString:@"20121210"];
+    self.appState.currentDate = [self.formatter dateFromString:yesterdayString];
+    AppState *updatedState = [self.appState navigatePreviousDay];
+    
+    STAssertNil(updatedState.currentWeek, @"Expected week to be nil, it is not");
 }
 
-- (NSDate *)yesterday
+- (void) testNavigatePreviousDay_mustSetCurrentDayToNil_givenNoPreviousDayExists
 {
-    return [self.formatter dateFromString:@"20121209"];
+    self.appState.currentDate = [self.formatter dateFromString:yesterdayString];
+    AppState *updatedState = [self.appState navigatePreviousDay];
+    
+    STAssertNil(updatedState.currentDay, @"Expected day to be nil, it is not");
 }
 
-- (NSDate *)tomorrow
+- (void) testNavigateNextDay_mustSetCurrentWeekToNil_givenNoNextDayExists
 {
-    return [self.formatter dateFromString:@"20121211"];
+    self.appState.currentDate = [self.formatter dateFromString:tomorrowString];
+    AppState *updatedState = [self.appState navigateNextDay];
+    
+    STAssertNil(updatedState.currentWeek, @"Expected week to be nil, it is not");
 }
 
+- (void) testNavigateNextDay_mustSetCurrentDayToNil_givenNoNextDayExists
+{
+    self.appState.currentDate = [self.formatter dateFromString:tomorrowString];
+    AppState *updatedState = [self.appState navigateNextDay];
+    
+    STAssertNil(updatedState.currentDay, @"Expected day to be nil, it is not");
+}
 
-- (Week *)testWeek
+- (void) testNavigateNextWeek_mustSetCurrentDateToCurrentWeekLastDatePlusOneDay
+{
+    AppState *updatedState = [self.appState navigateNextWeek];
+    STAssertDateStringEqualsDate(twoDaysFromNowString, updatedState.currentDate);
+};
+
+- (void) testNavigatePrevioustWeek_mustSetCurrentDateToCurrentWeekLastDateMinusOneDay
+{
+    AppState *updatedState = [self.appState navigatePreviousWeek];
+    STAssertDateStringEqualsDate(twoDaysAgoString, updatedState.currentDate);
+};
+
+- (Week *)createTestWeekWithYesterdayTodayAndTomorrow
 {
     Week *w = [[Week alloc] init];
 
-    Day *yesterday = [self createWithDate:[self yesterday]];
-    Day *today = [self createWithDate:[self today]];
-    Day *tomorow = [self createWithDate:[self tomorrow]];
+    Day *yesterday = [self createWithDate:[self.formatter dateFromString:yesterdayString]];
+    Day *today = [self createWithDate:[self.formatter dateFromString:todayString]];
+    Day *tomorow = [self createWithDate:[self.formatter dateFromString:tomorrowString]];
     
     w.days = [NSMutableArray arrayWithObjects:yesterday, today, tomorow, nil];
     
@@ -116,6 +159,5 @@
     
     return d;
 }
-
 
 @end
